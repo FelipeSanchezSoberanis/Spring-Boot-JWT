e1bb1244e7a5db53b7d573253b82461544e6e580
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2023-07-19 09:53:14 -0600

    Test accessing protected resource

--- src/test/java/com/felipe/jwtTest/JwtTestApplicationsTests.java
+++ src/test/java/com/felipe/jwtTest/JwtTestApplicationsTests.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -37,5 +39,31 @@ class JwtTestApplicationTests {
     assertEquals(HttpStatus.OK, loginResponse.getStatusCode());
     assertNotNull(loginResponse.getBody().getAccessToken());
     assertNotNull(loginResponse.getBody().getRefreshToken());
+  }
+
+  @Test
+  void testAccessingProtectedResource() {
+    try {
+      restTemplate.exchange(appUrl("/test/private"), HttpMethod.GET, null, Void.class);
+    } catch (HttpClientErrorException e) {
+      assertEquals(HttpStatus.FORBIDDEN, e.getStatusCode());
+    }
+
+    LoginRequest loginRequest = new LoginRequest("user", "password");
+    ResponseEntity<LoginResponse> loginResponse =
+        restTemplate.exchange(
+            appUrl("/auth/login"),
+            HttpMethod.POST,
+            new HttpEntity<>(loginRequest),
+            LoginResponse.class);
+
+    HttpHeaders httpHeaders = new HttpHeaders();
+    httpHeaders.add(
+        HttpHeaders.AUTHORIZATION, "Bearer " + loginResponse.getBody().getAccessToken());
+    ResponseEntity<Void> userResponse =
+        restTemplate.exchange(
+            appUrl("/test/private"), HttpMethod.GET, new HttpEntity<Void>(httpHeaders), Void.class);
+
+    assertEquals(HttpStatus.OK, userResponse.getStatusCode());
   }
 }
